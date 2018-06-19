a70601ae303d749200c953cb1420f44e66498363
Elisa Urda <urda.elisa@gmail.com>
2018-06-18 20:26:34 -0400

    header is displaying. No counter yet

---
+++ clickreact/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => (
+    <nav className="navbar navbar-dark bg-primary fixed-top">
+        <span className="navbar-text">Clicky... Office Style!</span>
+        <span className="navbar-text">Click an image to begin.</span>
+    </nav>
+);
+
+export default Header;
