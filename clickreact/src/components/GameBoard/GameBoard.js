7f29f79b591a76f53607eac1eaa6bc74090f761a
Elisa Urda <urda.elisa@gmail.com>
2018-06-18 21:04:16 -0400

    started putting together other components

---
+++ clickreact/src/components/GameBoard/GameBoard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import office from "/office.json";
+
+class GameBoard extends React.Component {
+    state = {
+        office
+    };
+
+    // create sort method to be passed to an on click event as a prop
+    // create counter for successful on click as well
+
+    // set up grip with a character component in each
+    // pass props to character component to show which character goes where based on array order
+
+};
+
+export default GameBoard;
