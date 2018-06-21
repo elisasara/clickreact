906e36c2141188c744eed9ea4ac5650a0d12420a
Elisa Urda <urda.elisa@gmail.com>
2018-06-21 18:15:23 -0400

    working on formatting

--- clickreact/src/components/Character/Character.js
+++ clickreact/src/components/Character/Character.js
@@ -1,11 +1,10 @@
 import React from "react";
+import "./Character.css";
 
 const Character = props => (
-<div className="card">
     <div className="img-container">
         <img  onClick={()=> props.handleClick(props.name)} alt={props.name} src={props.image} className="img-thumbnail" />
     </div>
-</div>
 )
 
 export default Character;
--- clickreact/src/components/Jumbotron/Jumbotron.js
+++ clickreact/src/components/Jumbotron/Jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Jumbotron.css";
 
 const Jumbotron = () => (
-    <div className="jumbotron">
+    <div className="jumbotron text-center">
         <h1>Clicky Game... Office Style!</h1>
         <img src="./images/theOfficePainting.jpg" alt="Office Painting" />
         <h3>Click on an image to earn points, but don't click any more than once!</h3>
