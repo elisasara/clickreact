aa6bfa883824427e1df96c55dd0fd59531536cd3
Elisa Urda <urda.elisa@gmail.com>
2018-06-20 20:55:27 -0400

    counter incrementing but only on second click event

--- clickreact/src/App.js
+++ clickreact/src/App.js
@@ -15,29 +15,41 @@ class App extends React.Component {
 
   handleClick = (name) => {
     const char = this.state.characters.find(chosen => chosen.name === name);
-    // const index = this.state.characters.indexOf({name: char});
-    char.clicked = true;
     console.log("Char: ", char);
-    console.log("clicked: ", char.clicked);
-    // console.log("Position:", index);
-
     // check to see if this.state.characters.clicked = true
     // if true, game over
-    // if (char.clicked) {
+    if (char.clicked) {
       // return component for game over
       // reset all characters to clicked: false
       // reset counter to 0
       // DO NOT RESET HGH SCORE
-    // }
+    }
     // if false, change to true
-    // else {
+    else {
       // set char.clicked to true
-      
+      char.clicked = true;
       // add a point to the counter
+      if (this.state.counter === this.state.highScore) {
+        this.setState({
+          counter: this.state.counter + 1,
+          highScore: this.state.highScore + 1
+        });
+      }
+      else {
+        this.setState({
+          counter: this.state.counter + 1
+        });
+      };
+      // console.log("clicked: ", char.clicked);
+      console.log("State:", this.state);  
       // if counter is higher than highscore then add a point to high score
       // call this.shuffleChar() function
-    // };
+    };
   };
+
+  resetClick = () => {
+
+  }
 
   shuffleChar = () => {
     const charArray = this.state.characters;
