73ebdfdeeeea943ad082846d281b63157c64512d
Elisa Urda <urda.elisa@gmail.com>
2018-06-21 20:32:11 -0400

    updated media query

--- clickreact/src/components/Jumbotron/Jumbotron.js
+++ clickreact/src/components/Jumbotron/Jumbotron.js
@@ -4,8 +4,10 @@ import "./Jumbotron.css";
 const Jumbotron = () => (
     <div className="jumbotron text-center">
         <h1>React Clicky Game... The Office Style!</h1>
-        <img src="./images/theOfficePainting.jpg" alt="Office Painting" />
+        <img src="./images/theOfficePainting.jpg" id="painting" alt="Office Painting" />
         <h3>Click on an image to earn points, but don't click any more than once!</h3>
+        <p>Made with ReactJS</p>
+        <img src="./images/logo.svg" alt="React Logo" id="reactLogo" />
     </div>
 );
 
